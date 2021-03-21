6f3233de8878a7dd5cf1f07e1c3c22cc59df2de1
avilesdiana <avilesdiana@outlook.com>
2021-03-20 18:09:05 -0600

    Tipos de datos JS

--- JAVASCRIPT/html5-boilerplate_v8.0.0/js/main.js
+++ JAVASCRIPT/html5-boilerplate_v8.0.0/js/main.js
@@ -1,1 +1,14 @@
 console.log("Mensaje a la consola");
+//alert("Hola mundo");
+
+//Tipos de datos
+var mensaje = "Es un mensaje";
+var count = 1;
+var pi = 3.1415;
+var esDivertido = true;
+
+console.log(mensaje);
+console.log(pi);
+console.log(count);
+console.log(esDivertido);
+
