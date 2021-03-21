529b726bcad3f0bd02a9010080f65db1f7359ada
avilesdiana <avilesdiana@outlook.com>
2021-03-20 19:20:48 -0600

    Instanceando objetos

--- JAVASCRIPT/html5-boilerplate_v8.0.0/js/main.js
+++ JAVASCRIPT/html5-boilerplate_v8.0.0/js/main.js
@@ -1,4 +1,4 @@
-/*
+
 console.log("Mensaje a la consola");
 //alert("Hola mundo");
 
@@ -13,19 +13,19 @@ console.log(pi);
 console.log(count);
 console.log(esDivertido); 
 
-*/
+
 
 //Usando use strict - no te permite utilizar variables no declaradas para código seguro
 //"use strict";
 
 
-var main = function(){
+var main = function () {
     "use strict";
 
     var mensaje = "HOLA MUNDO!",
-    count = 1, 
-    pi = 3.1416,
-    esDivertido = true;
+        count = 1,
+        pi = 3.1416,
+        esDivertido = true;
 
 
     console.log(mensaje)
@@ -44,7 +44,7 @@ console.error("Existe un error en tu programa");
 
 //console.clear();
 
-/*//Pruebas de rendimiento
+//Pruebas de rendimiento
 console.time("sumaArreglo");
     let numbers = [5,6,7,3,2,5,5,6,3,2,5,6,32,2,353,43,2,2,3423,3];
     let result = 0;
@@ -56,6 +56,34 @@ console.time("sumaArreglo");
 
 console.log(result);
 console.timeEnd("sumaArreglo");
-*/
 
 
+
+//Crear variables
+// Ya no se utiliza var, ahora es let y const
+
+"use strict";
+
+var unaVariable;
+let nombre;
+
+console.log(nombre);
+nombre = "Diana";
+
+console.log(nombre);
+
+nombre = "Laura"
+console.log(nombre);
+
+
+//Var nos permite declarar dos veces una variable y let no te lo permite, por eso es más recomendable
+
+const PI = 3.1416;
+
+console.log(PI);
+
+//Instanciando al objeto
+const menuElemento = document.getElementById("liElemento1")
+
+///console.log(menuElemento.innerText);
+console.log(menuElemento.innerHTML);
