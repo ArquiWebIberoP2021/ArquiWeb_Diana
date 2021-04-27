d7ac7962fa5dd314aee30a27c1903ff8f47e1869
avilesdiana <avilesdiana@outlook.com>
2021-04-27 02:08:00 -0500

    Terminado

--- PATRONES DISEÑO/ JavaScript./js/Visitor.js
+++ PATRONES DISEÑO/ JavaScript./js/Visitor.js
@@ -1,11 +1,9 @@
      //Visitor
-    //Interprete
+    //Visitante
 
-//Descripción
-
-
-var Employee = function (name, salary, vacation) {
-    var self = this;
+//Elements
+let Employee = function (name, salary, vacation) {
+    let self = this;
         
     this.accept = function (visitor) {
         visitor.visit(self);
@@ -32,22 +30,23 @@ var Employee = function (name, salary, vacation) {
     };
 };
  
-var ExtraSalary = function () {
+
+//Visitante
+let ExtraSalary = function () {
     this.visit = function (emp) {
         emp.setSalary(emp.getSalary() * 1.1);
     };
 };
  
-var ExtraVacation = function () {
+let ExtraVacation = function () {
     this.visit = function (emp) {
         emp.setVacation(emp.getVacation() + 2);
     };
 };
  
-// log helper
- 
-var log = (function() {
-    var log = "";
+// Registro Alert
+let log = (function() {
+    let log = "";
  
     return {
         add: function(msg) { log += msg + "\n"; },
@@ -57,22 +56,23 @@ var log = (function() {
  
 function run() {
         
-    var employees = [
-        new Employee("John", 10000, 10),
-        new Employee("Mary", 20000, 21),
-        new Employee("Boss", 250000, 51)
+        //ObjectStructure
+    let employees = [
+        new Employee("Justin", 10000, 10),
+        new Employee("Harry", 20000, 21),
+        new Employee("tanner", 250000, 51)
     ];
  
-    var visitorSalary = new ExtraSalary();
-    var visitorVacation = new ExtraVacation();
+    let visitorSalary = new ExtraSalary();
+    let visitorVacation = new ExtraVacation();
         
-    for (var i = 0, len = employees.length; i < len; i++) {
-        var emp = employees[i];
+    for (let i = 0, len = employees.length; i < len; i++) {
+        let emp = employees[i];
             
         emp.accept(visitorSalary);
         emp.accept(visitorVacation);
-        log.add(emp.getName() + ": $" + emp.getSalary() +
-            " and " + emp.getVacation() + " vacation days");
+        log.add(`${emp.getName()}: $${emp.getSalary()} y ${emp.getVacation()} días de vacaciones`);
+            
     }
  
     log.show();
